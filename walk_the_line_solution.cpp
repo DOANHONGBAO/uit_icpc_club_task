a335b895e3214207fca942b9b5d2b209425f8ac0
DoanHongBao <86391079+DOANHONGBAO@users.noreply.github.com>
2024-09-23 11:08:06 +0700

    sol_A

---
+++ walk_the_line_solution.cpp
@@ -0,0 +1,3 @@
+#Vì xe rùa có thể chở 1 người bất kỳ -> ta sẽ luôn chọn người có thời gian đi lâu nhát
+#Khi qua cầu cần có người quay về -> ta sẽ luôn chọn người có thời gian di chuyển thấp nhát
+#=> so sánh k và min({a1,...,an}) * (2 * (n - 1) - 1)
